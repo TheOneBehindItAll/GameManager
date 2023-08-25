5c51794fb202b78927fb28052e4971441b4a0cab
Mastermind <123602817+TheOneBehindItAll@users.noreply.github.com>
2023-08-25 18:50:10 -0400

    Added source files

---
+++ CustomGameManager/CustomGameManager/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace CustomGameManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Game1 = new System.Windows.Forms.Button();
+            this.Game2 = new System.Windows.Forms.Button();
+            this.Game3 = new System.Windows.Forms.Button();
+            this.Game4 = new System.Windows.Forms.Button();
+            this.Game5 = new System.Windows.Forms.Button();
+            this.Game10 = new System.Windows.Forms.Button();
+            this.Game9 = new System.Windows.Forms.Button();
+            this.Game8 = new System.Windows.Forms.Button();
+            this.Game7 = new System.Windows.Forms.Button();
+            this.Game6 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.reset = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Game1
+            // 
+            this.Game1.Location = new System.Drawing.Point(12, 12);
+            this.Game1.Name = "Game1";
+            this.Game1.Size = new System.Drawing.Size(182, 182);
+            this.Game1.TabIndex = 0;
+            this.Game1.Text = "Game 1";
+            this.Game1.UseVisualStyleBackColor = true;
+            this.Game1.Click += new System.EventHandler(this.Game1_Click);
+            // 
+            // Game2
+            // 
+            this.Game2.Location = new System.Drawing.Point(200, 12);
+            this.Game2.Name = "Game2";
+            this.Game2.Size = new System.Drawing.Size(182, 182);
+            this.Game2.TabIndex = 1;
+            this.Game2.Text = "Game 2";
+            this.Game2.UseVisualStyleBackColor = true;
+            this.Game2.Click += new System.EventHandler(this.Game2_Click);
+            // 
+            // Game3
+            // 
+            this.Game3.Location = new System.Drawing.Point(388, 12);
+            this.Game3.Name = "Game3";
+            this.Game3.Size = new System.Drawing.Size(182, 182);
+            this.Game3.TabIndex = 4;
+            this.Game3.Text = "Game 3";
+            this.Game3.UseVisualStyleBackColor = true;
+            this.Game3.Click += new System.EventHandler(this.Game3_Click);
+            // 
+            // Game4
+            // 
+            this.Game4.Location = new System.Drawing.Point(576, 12);
+            this.Game4.Name = "Game4";
+            this.Game4.Size = new System.Drawing.Size(182, 182);
+            this.Game4.TabIndex = 5;
+            this.Game4.Text = "Game 4";
+            this.Game4.UseVisualStyleBackColor = true;
+            this.Game4.Click += new System.EventHandler(this.Game4_Click);
+            // 
+            // Game5
+            // 
+            this.Game5.Location = new System.Drawing.Point(764, 12);
+            this.Game5.Name = "Game5";
+            this.Game5.Size = new System.Drawing.Size(182, 182);
+            this.Game5.TabIndex = 6;
+            this.Game5.Text = "Game 5";
+            this.Game5.UseVisualStyleBackColor = true;
+            this.Game5.Click += new System.EventHandler(this.Game5_Click);
+            // 
+            // Game10
+            // 
+            this.Game10.Location = new System.Drawing.Point(764, 200);
+            this.Game10.Name = "Game10";
+            this.Game10.Size = new System.Drawing.Size(182, 182);
+            this.Game10.TabIndex = 13;
+            this.Game10.Text = "Game 10";
+            this.Game10.UseVisualStyleBackColor = true;
+            this.Game10.Click += new System.EventHandler(this.Game10_Click);
+            // 
+            // Game9
+            // 
+            this.Game9.Location = new System.Drawing.Point(576, 200);
+            this.Game9.Name = "Game9";
+            this.Game9.Size = new System.Drawing.Size(182, 182);
+            this.Game9.TabIndex = 10;
+            this.Game9.Text = "Game 9";
+            this.Game9.UseVisualStyleBackColor = true;
+            this.Game9.Click += new System.EventHandler(this.Game9_Click);
+            // 
+            // Game8
+            // 
+            this.Game8.Location = new System.Drawing.Point(388, 200);
+            this.Game8.Name = "Game8";
+            this.Game8.Size = new System.Drawing.Size(182, 182);
+            this.Game8.TabIndex = 9;
+            this.Game8.Text = "Game 8";
+            this.Game8.UseVisualStyleBackColor = true;
+            this.Game8.Click += new System.EventHandler(this.Game8_Click);
+            // 
+            // Game7
+            // 
+            this.Game7.Location = new System.Drawing.Point(200, 200);
+            this.Game7.Name = "Game7";
+            this.Game7.Size = new System.Drawing.Size(182, 182);
+            this.Game7.TabIndex = 8;
+            this.Game7.Text = "Game 7";
+            this.Game7.UseVisualStyleBackColor = true;
+            this.Game7.Click += new System.EventHandler(this.Game7_Click);
+            // 
+            // Game6
+            // 
+            this.Game6.Location = new System.Drawing.Point(12, 200);
+            this.Game6.Name = "Game6";
+            this.Game6.Size = new System.Drawing.Size(182, 182);
+            this.Game6.TabIndex = 7;
+            this.Game6.Text = "Game 6";
+            this.Game6.UseVisualStyleBackColor = true;
+            this.Game6.Click += new System.EventHandler(this.Game6_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 388);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 28);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(118, 393);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(76, 23);
+            this.reset.TabIndex = 15;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(870, 393);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(76, 23);
+            this.Save.TabIndex = 16;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(961, 426);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Game10);
+            this.Controls.Add(this.Game9);
+            this.Controls.Add(this.Game8);
+            this.Controls.Add(this.Game7);
+            this.Controls.Add(this.Game6);
+            this.Controls.Add(this.Game5);
+            this.Controls.Add(this.Game4);
+            this.Controls.Add(this.Game3);
+            this.Controls.Add(this.Game2);
+            this.Controls.Add(this.Game1);
+            this.Name = "Form1";
+            this.Text = "Game Manager by therealmastermind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Game1;
+        private System.Windows.Forms.Button Game2;
+        private System.Windows.Forms.Button Game3;
+        private System.Windows.Forms.Button Game4;
+        private System.Windows.Forms.Button Game5;
+        private System.Windows.Forms.Button Game10;
+        private System.Windows.Forms.Button Game9;
+        private System.Windows.Forms.Button Game8;
+        private System.Windows.Forms.Button Game7;
+        private System.Windows.Forms.Button Game6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ CustomGameManager/CustomGameManager/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace CustomGameManager
+{
+    public partial class Form1 : Form
+    {
+        string[] games;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Game1.Text = Properties.Settings.Default.one;
+            Game2.Text = Properties.Settings.Default.two;
+            Game3.Text = Properties.Settings.Default.three;
+            Game4.Text = Properties.Settings.Default.four;
+            Game5.Text = Properties.Settings.Default.five;
+            Game6.Text = Properties.Settings.Default.six;
+            Game7.Text = Properties.Settings.Default.seven;
+            Game8.Text = Properties.Settings.Default.eight;
+            Game9.Text = Properties.Settings.Default.nine;
+            Game10.Text = Properties.Settings.Default.ten;
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text == "")
+            {
+                MessageBox.Show("Please Input The Game Slot You Would Like To Reset");
+            }
+
+            if (richTextBox1.Text != "")
+            {
+                if (richTextBox1.Text == "1")
+                    Game1.Text = "";
+                if (richTextBox1.Text == "2")
+                    Game2.Text = "";
+                if (richTextBox1.Text == "3")
+                    Game3.Text = "";
+                if (richTextBox1.Text == "4")
+                    Game4.Text = "";
+                if (richTextBox1.Text == "5")
+                    Game5.Text = "";
+                if (richTextBox1.Text == "6")
+                    Game6.Text = "";
+                if (richTextBox1.Text == "7")
+                    Game7.Text = "";
+                if (richTextBox1.Text == "8")
+                    Game8.Text = "";
+                if (richTextBox1.Text == "9")
+                    Game9.Text = "";
+                if (richTextBox1.Text == "10")
+                    Game10.Text = "";
+            }
+        }
+
+        private void Game1_Click(object sender, EventArgs e)
+        {
+            if(Game1.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game1.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game1.Text);
+            }
+        }
+
+        private void Game2_Click(object sender, EventArgs e)
+        {
+            if (Game2.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game2.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game2.Text);
+            }
+        }
+
+        private void Game3_Click(object sender, EventArgs e)
+        {
+            if (Game3.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game3.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game3.Text);
+            }
+        }
+
+        private void Game4_Click(object sender, EventArgs e)
+        {
+            if (Game4.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game4.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game4.Text);
+            }
+        }
+
+        private void Game5_Click(object sender, EventArgs e)
+        {
+            if (Game5.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game5.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game5.Text);
+            }
+        }
+
+        private void Game6_Click(object sender, EventArgs e)
+        {
+            if (Game6.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game6.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game6.Text);
+            }
+        }
+
+        private void Game7_Click(object sender, EventArgs e)
+        {
+            if (Game7.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game7.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game7.Text);
+            }
+        }
+
+        private void Game8_Click(object sender, EventArgs e)
+        {
+            if (Game8.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game8.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game8.Text);
+            }
+        }
+
+        private void Game9_Click(object sender, EventArgs e)
+        {
+            if (Game9.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game9.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game9.Text);
+            }
+        }
+
+        private void Game10_Click(object sender, EventArgs e)
+        {
+            if (Game10.Text == "")
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.ShowDialog();
+                Game10.Text = fd.FileName;
+            }
+            else
+            {
+                Process.Start(Game10.Text);
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.one = Game1.Text;
+            Properties.Settings.Default.two = Game2.Text;
+            Properties.Settings.Default.three = Game3.Text;
+            Properties.Settings.Default.four = Game4.Text;
+            Properties.Settings.Default.five = Game5.Text;
+            Properties.Settings.Default.six = Game6.Text;
+            Properties.Settings.Default.seven = Game7.Text;
+            Properties.Settings.Default.eight = Game8.Text;
+            Properties.Settings.Default.nine = Game9.Text;
+            Properties.Settings.Default.ten = Game10.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
